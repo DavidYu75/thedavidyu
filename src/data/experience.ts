2566a7d86b9ad4b0f45ba1190a251a3be7542c6e
David Yu <yu.dav@northeastern.edu>
2025-03-11 17:14:32 -0400

    formatting

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,26 +1,26 @@
 export interface Experience {
-    title: string;
-    period: string;
-    description: string;
-  }
+  title: string;
+  period: string;
+  description: string;
+}
   
-  export const experiences: Experience[] = [
-    {
-      title: 'Lead Software Engineer at Google',
-      period: 'Nov 2019 - Present',
-      description:
-        "As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.",
-    },
-    {
-      title: 'Software Engineer at Youtube',
-      period: 'Jan 2017 - Oct 2019',
-      description:
-        "At Youtube, I served as a Software Engineer, focusing on the design and implementation of backend systems for the social media giant's dynamic platform. Working on projects that involved large scale data processing and user engagement features.",
-    },
-    {
-      title: 'Junior Software Engineer at Apple',
-      period: 'Jan 2016 - Dec 2017',
-      description:
-        "During my tenure at Apple, I held the role of Software Architect, where I played a key role in shaping the architecture of mission-critical software projects. Responsible for designing scalable and efficient systems.",
-    },
-  ];
+export const experiences: Experience[] = [
+  {
+    title: 'Lead Software Engineer at Google',
+    period: 'Nov 2019 - Present',
+    description:
+      "As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.",
+  },
+  {
+    title: 'Software Engineer at Youtube',
+    period: 'Jan 2017 - Oct 2019',
+    description:
+      "At Youtube, I served as a Software Engineer, focusing on the design and implementation of backend systems for the social media giant's dynamic platform. Working on projects that involved large scale data processing and user engagement features.",
+  },
+  {
+    title: 'Junior Software Engineer at Apple',
+    period: 'Jan 2016 - Dec 2017',
+    description:
+      "During my tenure at Apple, I held the role of Software Architect, where I played a key role in shaping the architecture of mission-critical software projects. Responsible for designing scalable and efficient systems.",
+  },
+];
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,42 +1,42 @@
 export interface Project {
-    id: string;
-    title: string;
-    description: string;
-    image: string;
-    link: string;
-  }
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
   
-  export const projects: Project[] = [
-    {
-      id: '01',
-      title: 'Black-Scholes Model',
-      description:
-        'A sophisticated financial tool implementing the Black-Scholes mathematical model to accurately price options contracts and analyze market volatility.',
-      image: '/images/inprogress.png',
-      link: '#',
-    },
-    {
-      id: '02',
-      title: 'IntReview',
-      description:
-        'An AI-powered interview preparation platform that provides real-time feedback on verbal and non-verbal communication using computer vision and speech analysis.',
-      image: '/images/intreview.png',
-      link: 'https://github.com/DavidYu75/intreview',
-    },
-    {
-      id: '03',
-      title: 'Classify',
-      description:
-        'A machine learning application that intelligently categorizes Spotify playlists into themed collections to enhance music discovery through audio feature analysis.',
-      image: '/images/classify.png',
-      link: "https://github.com/thaninbew/classify",
-    },
-    {
-      id: '04',
-      title: 'PlateMate',
-      description:
-        'A restaurant analytics platform leveraging data-driven insights to help owners optimize menu offerings, pricing, and inventory management for increased profitability.',
-      image: '/images/inprogress.png',
-      link: "https://github.com/GenerateNU/platemate",
-    },
-  ];
+export const projects: Project[] = [
+  {
+    id: '01',
+    title: 'Black-Scholes Model',
+    description:
+      'A sophisticated financial tool implementing the Black-Scholes mathematical model to accurately price options contracts and analyze market volatility.',
+    image: '/images/inprogress.png',
+    link: '#',
+  },
+  {
+    id: '02',
+    title: 'IntReview',
+    description:
+      'An AI-powered interview preparation platform that provides real-time feedback on verbal and non-verbal communication using computer vision and speech analysis.',
+    image: '/images/intreview.png',
+    link: 'https://github.com/DavidYu75/intreview',
+  },
+  {
+    id: '03',
+    title: 'Classify',
+    description:
+      'A machine learning application that intelligently categorizes Spotify playlists into themed collections to enhance music discovery through audio feature analysis.',
+    image: '/images/classify.png',
+    link: "https://github.com/thaninbew/classify",
+  },
+  {
+    id: '04',
+    title: 'PlateMate',
+    description:
+      'A restaurant analytics platform leveraging data-driven insights to help owners optimize menu offerings, pricing, and inventory management for increased profitability.',
+    image: '/images/inprogress.png',
+    link: "https://github.com/GenerateNU/platemate",
+  },
+];
