b6078980a306b95205c6b8db51aeae0daf611167
David Yu <yu.dav@northeastern.edu>
2025-03-12 00:12:06 -0400

    detailed experience pages

---
+++ src/app/experience/[slug]/page.tsx
@@ -0,0 +1,34 @@
+import ExperienceDetail from '@/components/ExperienceDetail';
+import { experiences } from '@/data/experience';
+import { Metadata } from 'next';
+
+type Props = {
+  params: { slug: string }
+};
+
+export async function generateMetadata(
+  { params }: Props): Promise<Metadata> {
+  const slug = params.slug;
+  const experience = experiences.find(exp => exp.slug === slug);
+  
+  if (!experience) {
+    return {
+      title: 'Experience Not Found',
+    };
+  }
+
+  return {
+    title: `${experience.title} at ${experience.company} | David Yu`,
+    description: experience.description,
+  };
+}
+
+export function generateStaticParams() {
+  return experiences.map((experience) => ({
+    slug: experience.slug,
+  }));
+}
+
+export default function ExperiencePage() {
+  return <ExperienceDetail />;
+}
